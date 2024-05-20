9498b00d70c3162d8111c63dd35eefcb85936093
Arun Kumar R <aruntutter19@gmail.com>
2024-05-20 23:10:47 +0530

    carousel functionality added

--- project01-carousel/scripts/script.js
+++ project01-carousel/scripts/script.js
@@ -1,1 +1,130 @@
 "use strict";
+
+const JSCarousel = ({ carouselSelector, slideSelector }) => {
+  let currentSlideIndex = 0;
+  let prevBtn, nextBtn;
+
+  const carousel = document.querySelector(carouselSelector);
+
+  if (!carousel) {
+    console.error("Specify a valid selector for the carousel.");
+    return null;
+  }
+
+  const slides = carousel.querySelectorAll(slideSelector);
+
+  if (!slides.length) {
+    console.error("Specify a valid selector for slides.");
+    return null;
+  }
+
+  const addElement = (tag, attributes, children) => {
+    const element = document.createElement(tag);
+
+    if (attributes) {
+      Object.entries(attributes).forEach(([key, value]) => {
+        element.setAttribute(key, value);
+      });
+    }
+
+    if (children) {
+      if (typeof children === "string") {
+        element.textContent = children;
+      } else {
+        children.forEach((child) => {
+          if (typeof child === "string") {
+            element.appendChild(document.createTextNode(child));
+          } else {
+            element.appendChild(child);
+          }
+        });
+      }
+    }
+
+    return element;
+  };
+
+  const tweakStructure = () => {
+    carousel.setAttribute("tabindex", "0");
+
+    const carouselInner = addElement("div", {
+      class: "carousel-inner",
+    });
+    carousel.insertBefore(carouselInner, slides[0]);
+
+    slides.forEach((slide, index) => {
+      carouselInner.appendChild(slide);
+      slide.style.transform = `translateX(${index * 100}%)`;
+    });
+
+    prevBtn = addElement(
+      "btn",
+      {
+        class: "carousel-btn carousel-btn--prev-next carousel-btn--prev",
+        "aria-label": "Previous Slide",
+      },
+      "<"
+    );
+    carouselInner.appendChild(prevBtn);
+
+    nextBtn = addElement(
+      "btn",
+      {
+        class: "carousel-btn carousel-btn--prev-next carousel-btn--next",
+        "aria-label": "Next Slide",
+      },
+      ">"
+    );
+    carouselInner.appendChild(nextBtn);
+  };
+
+  const adjustSlidePosition = () => {
+    slides.forEach((slide, i) => {
+      slide.style.transform = `translateX(${100 * (i - currentSlideIndex)}%)`;
+    });
+  };
+
+  const updateCarouselState = () => {
+    adjustSlidePosition();
+  };
+
+  const moveSlide = (direction) => {
+    const newSlideIndex =
+      direction === "next"
+        ? (currentSlideIndex + 1) % slides.length
+        : (currentSlideIndex - 1 + slides.length) % slides.length;
+    currentSlideIndex = newSlideIndex;
+    updateCarouselState();
+  };
+
+  const handlePrevBtnClick = () => moveSlide("prev");
+  const handleNextBtnClick = () => moveSlide("next");
+
+  const attachEventListeners = () => {
+    prevBtn.addEventListener("click", handlePrevBtnClick);
+    nextBtn.addEventListener("click", handleNextBtnClick);
+  };
+
+  const create = () => {
+    tweakStructure();
+    attachEventListeners();
+  };
+
+  const destroy = () => {
+    prevBtn.removeEventListener("click", handlePrevBtnClick);
+    nextBtn.removeEventListener("click", handleNextBtnClick);
+  };
+
+  return { create, destroy };
+};
+
+const carousel1 = JSCarousel({
+  carouselSelector: "#carousel-1",
+  slideSelector: ".slide",
+});
+
+carousel1.create();
+
+window.addEventListener("unload", () => {
+  carousel1.destroy();
+});
