25f618f26b9371f0e492cbb93ea9236feee06fdb
Arun Kumar R <aruntutter19@gmail.com>
2024-05-29 12:26:29 +0530

    calculator functionality added

--- project02-calculator/scripts/script.js
+++ project02-calculator/scripts/script.js
@@ -1,11 +1,28 @@
 "use strict";
 
-// global variables
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
-// functions
-const init = () => {};
+// function to calculate
+const calculate = (btnValue) => {
+  display.focus();
 
-//event listeners
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
-// initial settings
-init();
+// Add event listeners to buttons
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
