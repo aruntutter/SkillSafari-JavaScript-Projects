5ec305c70b61826f6563b3b2d2d40eb2a8aa3574
Arun Kumar R <aruntutter19@gmail.com>
2024-05-29 17:22:31 +0530

    clock logic added

--- project05-digital_clock/scripts/script.js
+++ project05-digital_clock/scripts/script.js
@@ -1,11 +1,25 @@
 "use strict";
 
-// global variables
+let ampm = document.getElementById("ampm");
+function displayTime() {
+  let dateTime = new Date();
+  let hr = dateTime.getHours();
+  let min = padZero(dateTime.getMinutes());
+  let sec = padZero(dateTime.getSeconds());
+  if (hr > 12) {
+    ampm.innerHTML = "PM";
+    hr = hr - 12;
+  } else {
+    ampm.innerHTML = "AM";
+  }
+  hr = padZero(hr);
+  document.getElementById("hours").innerHTML = hr;
+  document.getElementById("mins").innerHTML = min;
+  document.getElementById("seconds").innerHTML = sec;
+}
 
-// functions
-const init = () => {};
+function padZero(num) {
+  return num < 10 ? "0" + num : num;
+}
 
-//event listeners
-
-// initial settings
-init();
+setInterval(displayTime, 100);
